26419b80bc26437fb6bf48b60e3bd378bd581cdb
CharBerg <charlesbergeron@cmail.carleton.ca>
2015-02-06 21:37:21 -0500

    Basic file IO, starting to read train data

--- src/Test.java
+++ src/Test.java
@@ -2,25 +2,42 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Test {
 	
 	private static final String DATAFILE = "Testing.txt";
+	//private static final String DATACOLUMNS
 	
-	public void readData(){
+	private int lineNumber = 0;
+	private Train[] trains = null;
+	
+	public void readTrainData() throws IOException{
 		
 		File dataFile = new File(DATAFILE);
+		BufferedReader br = null;
+		
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(dataFile));
+			br = new BufferedReader(new FileReader(dataFile));
 		} catch (FileNotFoundException e) {
 			System.out.println("");
+			System.exit(0);
+		}
+		
+		String in = br.readLine();
+		in = br.readLine();
+		//Read individual words on a line, regardless of type of white-space
+		String[] s = in.split("\\s+");
+		
+		for (String word:s) {
+			System.out.println(word);
 		}
 		
 	}
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("David Test Push");
+	public static void main(String[] args) throws IOException {
+		Test t = new Test();
+		t.readTrainData();
 	}
 
 }
