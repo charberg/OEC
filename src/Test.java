8fbea77d3ea0dc8cf70158de9015f2c2d72d0987
CharBerg <charlesbergeron@cmail.carleton.ca>
2015-02-06 23:26:02 -0500

    Complete reading-in of train data functions, number of trains function, open file function

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Test {
 	
@@ -10,12 +11,21 @@ public class Test {
 	//private static final String DATACOLUMNS
 	
 	private int lineNumber = 0;
-	private Train[] trains = null;
+	public int numTrains = -1;
+	public ArrayList<Train> trains = null;
+	private File dataFile;
+	private BufferedReader br;
 	
-	public void readTrainData() throws IOException{
+	public Test() throws IOException{
+		dataFile = new File(DATAFILE);
+		openFile();
+		this.findNumTrains();
+		openFile();	//Re-open file for train data reading
+		String x = br.readLine();	//Clear the column names row
 		
-		File dataFile = new File(DATAFILE);
-		BufferedReader br = null;
+	}
+	
+	private void openFile() {
 		
 		try {
 			br = new BufferedReader(new FileReader(dataFile));
@@ -24,20 +34,94 @@ public class Test {
 			System.exit(0);
 		}
 		
-		String in = br.readLine();
-		in = br.readLine();
-		//Read individual words on a line, regardless of type of white-space
-		String[] s = in.split("\\s+");
+	}
+	
+	private void findNumTrains() throws IOException{
 		
-		for (String word:s) {
-			System.out.println(word);
+		if(numTrains == -1){	//If number of trains not yet determined
+			int setTime = -1;
+			int newTime;
+			int lineNum = 0;
+			boolean timeCheck = true;
+			
+			String x = br.readLine();	//Clear the column names line
+			x = br.readLine();
+			
+			while(timeCheck == true){
+				//Read individual words on a line, regardless of type of white-space
+				
+				String[] trainDataLine = x.split("\\s+");
+				
+				if(setTime == -1){	//If reading the first line
+					setTime = Integer.parseInt(trainDataLine[0]);
+				}
+				
+				else {	//If a time value has already been read in
+					newTime = Integer.parseInt(trainDataLine[0]);
+					if(newTime != setTime) {	//If found new pulse time, stop reading
+						timeCheck = false;
+						numTrains = lineNum;
+					}
+					
+				}
+				
+				if(timeCheck == true) {
+					lineNum++;
+					x = br.readLine();
+				}
+				
+			}
+		}
+		
+	}
+	
+	public void readTrainData() throws IOException{
+		
+		String in;
+			
+			//Read individual words on a line, regardless of type of white-space
+		
+			
+		if(trains == null) {	//If first time reading in train data
+			ArrayList<Train> t = new ArrayList<Train>();
+			for(int i = 0; i < numTrains; i++) {		
+				in = br.readLine();
+				String[] trainDataLine = in.split("\\s+");
+				t.add(new Train(Integer.parseInt(trainDataLine[1]),
+						Integer.parseInt(trainDataLine[2]),
+						Integer.parseInt(trainDataLine[3]),
+						trainDataLine[4],
+						Integer.parseInt(trainDataLine[5]),
+						trainDataLine[6]));
+				
+				trains = t;
+			}
+		}
+			
+		else {
+			for(int i = 0; i < numTrains; i++) {
+				in = br.readLine();
+				String[] trainDataLine = in.split("\\s+");
+				trains.get(i).setFrtLoc(Integer.parseInt(trainDataLine[2]));
+				trains.get(i).setRearLoc(Integer.parseInt(trainDataLine[3]));
+				trains.get(i).setTrack(trainDataLine[4]);
+				trains.get(i).setSpeed(Integer.parseInt(trainDataLine[5]));
+				trains.get(i).setStatus(trainDataLine[6]);
+			}
 		}
 		
 	}
 
 	public static void main(String[] args) throws IOException {
 		Test t = new Test();
-		t.readTrainData();
+		for(int i = 0; i < 5; i++) {
+			t.readTrainData();
+			for(Train test : t.trains) {
+				System.out.println(test.toString());
+			}
+			System.out.println("/////////////////////////////////////");
+		}
+
 	}
 
 }
