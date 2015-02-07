938d692454ddb1f48a949dbdeeca946228dac938
CharBerg <charlesbergeron@cmail.carleton.ca>
2015-02-06 20:58:03 -0500

    Initial commit

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+define
 
 public class Test {
+	
+	private static final String DATAFILE = "Testing.txt";
+	
+	public void readData(){
+		
+		File dataFile = new File(DATAFILE);
+		BufferedReader br = new BufferedReader(new FileReader())
+		
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
